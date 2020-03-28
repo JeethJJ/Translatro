32e358f3319d915616ee8784369fdd8712c3b075
Jihan Jeeth <jeeth.2018453@iit.ac.lk>
2020-03-28 19:16:04 +0530

    Finished the main activity and entrance page

---
+++ app/src/main/java/github/jeethjj/translatro/MainActivity.java
@@ -0,0 +1,27 @@
+package github.jeethjj.translatro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REDIRECT_TIME = 2000;  // this activity will be shown only for two seconds end it will be redirected automatically to the homepage
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {  //to redirect to home page from main activity in 2s
+                Intent intend = new Intent(MainActivity.this, Home.class);
+                startActivity(intend);
+                overridePendingTransition(R.anim.fade_out,R.anim.fade_in);
+                finish();
+            }
+        }, REDIRECT_TIME);
+    }
+}
