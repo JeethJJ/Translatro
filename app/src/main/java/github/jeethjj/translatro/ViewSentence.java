e5fb57891c350bfb8e1471c017c37128887358e2
Jihan Jeeth <jeeth.2018453@iit.ac.lk>
2020-03-29 00:37:12 +0530

    First 5 activities created, both lanscape and portrait

---
+++ app/src/main/java/github/jeethjj/translatro/ViewSentence.java
@@ -0,0 +1,36 @@
+package github.jeethjj.translatro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ViewSentence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_sentence);
+
+//        ListView lv = findViewById(R.id.list);
+//        String[] festivals = {
+//                "Diwali",
+//                "Holi",
+//                "Christmas",
+//                "Eid",
+//                "Baisakhi",
+//                "Halloween"
+//        };
+//        TextView textView = findViewById(R.id.textView);
+//
+//        final ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item, R.id.textView, festivals);
+//
+//        lv.setAdapter(adapter);
+
+//        https://stackoverflow.com/questions/17339762/how-to-convert-settext-to-a-listview-in-android/17340026
+
+//        https://www.studytonight.com/android/android-listview
+    }
+}
