ebc73240a8ebe45e26362b76c1260084443e3751
Jihan Jeeth <jeeth.2018453@iit.ac.lk>
2020-03-31 21:11:18 +0530

    The main layouts done

--- app/src/main/java/github/jeethjj/translatro/Home.java
+++ app/src/main/java/github/jeethjj/translatro/Home.java
@@ -2,7 +2,9 @@ package github.jeethjj.translatro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +12,41 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void addPhrase(View view) {
+        Intent intend = new Intent(Home.this, Add.class);
+        startActivity(intend);
+        overridePendingTransition(R.anim.fade_out,R.anim.fade_in);
+    }
+
+    public void disPhrase(View view) {
+        Intent intend = new Intent(Home.this, ViewSentence.class);
+        startActivity(intend);
+        overridePendingTransition(R.anim.fade_out,R.anim.fade_in);
+    }
+
+    public void editPhrase(View view) {
+        Intent intend = new Intent(Home.this, Edit.class);
+        startActivity(intend);
+        overridePendingTransition(R.anim.fade_out,R.anim.fade_in);
+    }
+
+    public void langSub(View view) {
+        Intent intend = new Intent(Home.this, Subscriptions.class);
+        startActivity(intend);
+        overridePendingTransition(R.anim.fade_out,R.anim.fade_in);
+    }
+
+    public void translate(View view) {
+        Intent intend = new Intent(Home.this, Translate.class);
+        startActivity(intend);
+        overridePendingTransition(R.anim.fade_out,R.anim.fade_in);
+    }
+
+    public void savedTrans(View view) {
+        Intent intend = new Intent(Home.this, Saved.class);
+        startActivity(intend);
+        overridePendingTransition(R.anim.fade_out,R.anim.fade_in);
     }
 }
--- app/src/main/java/github/jeethjj/translatro/ViewSentence.java
+++ app/src/main/java/github/jeethjj/translatro/ViewSentence.java
@@ -5,29 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ViewSentence extends AppCompatActivity {
+
+    ArrayList<String> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_sentence);
 
-//        ListView lv = findViewById(R.id.list);
-//        String[] festivals = {
-//                "Diwali",
-//                "Holi",
-//                "Christmas",
-//                "Eid",
-//                "Baisakhi",
-//                "Halloween"
-//        };
-//        TextView textView = findViewById(R.id.textView);
-//
-//        final ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item, R.id.textView, festivals);
-//
-//        lv.setAdapter(adapter);
+        ListView lv = findViewById(R.id.list_all_phrase);
+        String[] festivals = {
+                "Diwali",
+                "Holi",
+                "Christmas",
+                "Eid",
+                "Baisakhi",
+                "Diwali",
+                "Holi",
+                "Christmas",
+                "Eid",
+                "Baisakhi",
+                "Diwali",
+                "Holi",
+                "Christmas",
+                "Eid",
+                "Baisakhi",
+                "Diwali",
+                "Holi",
+                "Christmas",
+                "Eid",
+                "Baisakhi",
+                "Halloween"
+        };
+        itemList=new ArrayList<String>(Arrays.asList(festivals));
+
+//        TextView textView = findViewById(R.id.txtview);
+
+//        final ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_textview, R.id.textView, festivals);
+
+        ArrayAdapter adapter=new ArrayAdapter<String>(this,R.layout.list_textview,R.id.text_view,itemList);
+
+        lv.setAdapter(adapter);
 
 //        https://stackoverflow.com/questions/17339762/how-to-convert-settext-to-a-listview-in-android/17340026
 
